2188bd2d5945074fa26ac0abc23347f7c1949fc3
Matt Keller <matt@zensight.co>
2015-04-27 17:42:33 -0400

    Update clojars group

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject file-buffer "0.1.0"
+(defproject co.zensight/file-buffer "0.1.0"
   :description "A buffer for large data volumes backed by a file."
   :url "https://github.com/Zensight/file-buffer"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.html"}
+  :scm {:name "git"
+        :url "https://github.com/Zensight/file-buffer"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :aot [zensight.file-buffer.core]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
