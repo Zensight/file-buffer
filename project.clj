761a8f77bf4bcd788d17287ffeaac1c900727399
Matt Keller <mattkeller@gmail.com>
2015-06-25 06:56:50 -0400

    Bump to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject co.zensight/file-buffer "0.1.1"
+(defproject co.zensight/file-buffer "0.1.2-SNAPSHOT"
   :description "A buffer for large data volumes backed by a file."
   :url "https://github.com/Zensight/file-buffer"
   :license {:name "The MIT License (MIT)"
